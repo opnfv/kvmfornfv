145d38de68b3d1a1f08e869d60d799af1a5b69d4
Yunhong Jiang <yunhong.jiang@intel.com>
2015-08-04 14:24:36 -0700

    A script to build the opnfv kernel
    This script simply copy the kernel config to the target directory
    then build the kernel.
    
    It accept the parameter and passing directly to the kernel buidling.
    
    Change-Id: I26cdda00696d2916546ffb16ec427edddd420eb6
    Signed-off-by: Yunhong Jiang <yunhong.jiang@intel.com>

---
+++ tools/rtlinux-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+XTRACE=$(set +o | grep xtrace)
+set +o xtrace
+
+TOP_DIR=$(cd $(dirname "$0")/.. && pwd)
+
+if [ ! -f $TOP_DIR/kernel/.config ]; then
+	cp $TOP_DIR/tools/configs/opnfv.config $TOP_DIR/kernel/.config
+fi
+pushd $TOP_DIR/kernel &>/dev/null
+
+JN=`lscpu |grep "^CPU(s)" |cut -d ':' -f 2`
+make -j $JN $@
+
+popd &> /dev/null
+
+$XTRACE

ff3b1942a38de3a9b37baba19b01f14be9bd9b34
darwei <david.w.su@intel.com>
2015-08-20 16:12:47 -0700

    Add build script for continuous integration
    The script is based on the build script for copper
    https://git.opnfv.org/cgit/releng/tree/jjb/copper/docu-build.sh
    The script should be run from the root of kvmfornfv source tree
    
    Change-Id: Ic12890990741441e88e35a87ef2570b1c601ca61

---
+++ ci/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ ${#} -ne 1 ] ; then
+    echo "usage: ${0} output_dir"
+    exit 1
+fi
+
+if [ ! -d ${1} -o ! -w ${1} ] ; then
+    echo "${0}: Output directory '${1}' does not exist or cannot be written"
+    exit 1
+fi
+output_dir=${1}
+
+kernel_src_dir=kernel
+if [ ! -d ${kernel_src_dir} ] ; then
+    echo "${0}: Directory '${kernel_src_dir}' does not exist, run this script from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+cd ${kernel_src_dir}
+
+echo
+echo "Build"
+echo "-----"
+echo
+
+config_file=arch/x86/configs/opnfv.config
+if [ ! -f ${config_file} ] ; then
+    echo "${0}: ${config_file} does not exist"
+    exit 1
+fi
+cp -f ${config_file} .config
+
+rpmbuild_dir=/tmp/kvmfornfv_rpmbuild.$$
+artifact_dir=${rpmbuild_dir}/RPMS/x86_64
+
+rm -rf ${rpmbuild_dir}
+
+make RPMOPTS="--define '_topdir ${rpmbuild_dir}'" rpm-pkg
+if [ ${?} -ne 0 ] ; then
+    echo "${0}: Kernel build failed"
+    exit 1
+fi
+
+cp -f ${artifact_dir}/* ${output_dir}
+
+rm -rf ${rpmbuild_dir}

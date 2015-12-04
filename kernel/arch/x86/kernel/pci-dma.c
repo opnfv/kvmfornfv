78574a628265732b68a9d659f7d3bcf0ceb0bd97
José Pekkarinen <jose.pekkarinen@nokia.com>
2015-12-04 14:21:14 +0200

    Add menu choice to select the default setup for IOMMU.
    Change-Id: I8a8e72f51fdf2255a360f80088750f72b3b738f0
    Signed-off-by: José Pekkarinen <jose.pekkarinen@nokia.com>

--- kernel/arch/x86/kernel/pci-dma.c
+++ kernel/arch/x86/kernel/pci-dma.c
@@ -146,63 +146,61 @@ void dma_generic_free_coherent(struct device *dev, size_t size, void *vaddr,
  */
 static __init int iommu_setup(char *p)
 {
+	char *default_setup = (!*p) ? CONFIG_IOMMU_DEFAULT : p;
 	iommu_merge = 1;
 
-	if (!p)
-		return -EINVAL;
-
-	while (*p) {
-		if (!strncmp(p, "off", 3))
+	while (*default_setup) {
+		if (!strncmp(default_setup, "off", 3))
 			no_iommu = 1;
 		/* gart_parse_options has more force support */
-		if (!strncmp(p, "force", 5))
+		if (!strncmp(default_setup, "force", 5))
 			force_iommu = 1;
-		if (!strncmp(p, "noforce", 7)) {
+		if (!strncmp(default_setup, "noforce", 7)) {
 			iommu_merge = 0;
 			force_iommu = 0;
 		}
 
-		if (!strncmp(p, "biomerge", 8)) {
+		if (!strncmp(default_setup, "biomerge", 8)) {
 			iommu_merge = 1;
 			force_iommu = 1;
 		}
-		if (!strncmp(p, "panic", 5))
+		if (!strncmp(default_setup, "panic", 5))
 			panic_on_overflow = 1;
-		if (!strncmp(p, "nopanic", 7))
+		if (!strncmp(default_setup, "nopanic", 7))
 			panic_on_overflow = 0;
-		if (!strncmp(p, "merge", 5)) {
+		if (!strncmp(default_setup, "merge", 5)) {
 			iommu_merge = 1;
 			force_iommu = 1;
 		}
-		if (!strncmp(p, "nomerge", 7))
+		if (!strncmp(default_setup, "nomerge", 7))
 			iommu_merge = 0;
-		if (!strncmp(p, "forcesac", 8))
+		if (!strncmp(default_setup, "forcesac", 8))
 			iommu_sac_force = 1;
-		if (!strncmp(p, "allowdac", 8))
+		if (!strncmp(default_setup, "allowdac", 8))
 			forbid_dac = 0;
-		if (!strncmp(p, "nodac", 5))
+		if (!strncmp(default_setup, "nodac", 5))
 			forbid_dac = 1;
-		if (!strncmp(p, "usedac", 6)) {
+		if (!strncmp(default_setup, "usedac", 6)) {
 			forbid_dac = -1;
 			return 1;
 		}
 #ifdef CONFIG_SWIOTLB
-		if (!strncmp(p, "soft", 4))
+		if (!strncmp(default_setup, "soft", 4))
 			swiotlb = 1;
 #endif
-		if (!strncmp(p, "pt", 2))
+		if (!strncmp(default_setup, "pt", 2))
 			iommu_pass_through = 1;
 
-		gart_parse_options(p);
+		gart_parse_options(default_setup);
 
 #ifdef CONFIG_CALGARY_IOMMU
-		if (!strncmp(p, "calgary", 7))
+		if (!strncmp(default_setup, "calgary", 7))
 			use_calgary = 1;
 #endif /* CONFIG_CALGARY_IOMMU */
 
-		p += strcspn(p, ",");
-		if (*p == ',')
-			++p;
+		default_setup += strcspn(default_setup, ",");
+		if (*default_setup == ',')
+			++default_setup;
 	}
 	return 0;
 }

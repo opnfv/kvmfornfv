c715b6029fd5b4eaf323f5efde4ec5db5ba0a9b4
davidjchou <david.j.chou@intel.com>
2016-06-14 08:04:29 -0700

    Rebase fuel-plugin for kvmfornfv to fuel 9.0
    1. Uplifit fuel-plugin for kvmfornfv to fuel 9.0 due to that fuel 9.0 will be the
       deployment tool of OPNFV Colorado release.
    2. Fixed quirk for kernel.
    3. Added all tools and libs for building OVS module.
    4. Allow KVM developer to build fuel-plugin-kvm after they modify the kerenl
       code without commiting their change into the repo first.  So, they can test
       their code change by fuel-plugin-kvm till they satisfy with their change,
       then commit to the repo.
    5. The final code built into OPNFV fuel iso will depend on the commit ID for
       kvmfornfv speicifed in fuel for OPNFV source tree.
    
    Change-Id: Iaf9ff49c69df374d0757884cfdac4cccee3eebe4
    Signed-off-by: davidjchou <david.j.chou@intel.com>

--- fuel-plugin/vagrant/build_fuel_plugin.sh
+++ fuel-plugin/vagrant/build_fuel_plugin.sh
@@ -9,4 +9,11 @@ sudo apt-get install ruby -y
 sudo gem install rubygems-update
 sudo gem install fpm
 sudo apt-get install docker.io -y
-cd /kvmfornfv/fuel-plugin; fpb --debug --build .
+cd /home/vagrant
+# Will build fuel-plugin-kvm in guest VM local directory, not change host
+cp -r /kvmfornfv .
+cd kvmfornfv/fuel-plugin
+fpb --debug --build .
+# Copy the built fuel-plugin-kvm back to the host
+rm /kvmfornfv/fuel-plugin/fuel-plugin-kvm*.rpm
+cp fuel-plugin-kvm*.rpm /kvmfornfv/fuel-plugin/.

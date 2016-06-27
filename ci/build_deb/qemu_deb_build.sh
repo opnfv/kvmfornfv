13ac42a563ab733b6a99fd81d92ac55cd1b4e6d1
Swati <swatix.sharma@intel.com>
2016-06-27 17:53:53 +0530

    OPNFV KVM4NFV CICD: Scripts for creating Rpms & Debians for Kernel, Qemu
    This patch contains the files for generating rpms & debians for both kernel
    & qemu changes, as part of kvmfornfv project development. The scripts will be
    triggered to build kernel-rpm & qemu-rpm packages inside centos docker, and
    kernel-debian & qemu-debian packages inside ubuntu docker, as part of the
    CICD process.
    After the new builds are ready, they will be deployed on the pharos testbed
    for verification. Later, these will be consumed/triggered by verify/daily
    Releng JJBs.
    
    Reference: https://wiki.opnfv.org/display/kvm/KVM4NFV+CICD+Jobs
    
    Change-Id: Ie8e8108566790a6c8db2fef1c9b5799a41732b44
    
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

---
+++ ci/build_deb/qemu_deb_build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+qemu_src_dir=qemu
+workspace=/root
+debbuild_dir=$workspace/debbuild
+scripts_dir=ci/build_qemu
+output_dir="$1"
+VERSION=`grep -m 1 "VERSION"  ${qemu_src_dir}/config-host.mak | cut -d= -f2-`
+
+usage () {
+    echo "usage: ${0} output_dir"
+    exit 1
+}
+
+if [[ -z "$@" ]]; then
+    usage
+fi
+
+if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+    echo "${0}: Output directory '${output_dir}' does not exist or cannot be written"
+    exit 1
+fi
+
+if [ ! -d ${qemu_src_dir} ] ; then
+    echo "${0}: Directory '${qemu_src_dir}' does not exist, run this script from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+echo
+echo "Build"
+echo
+
+qemu_deb_build() {
+    mkdir -p $debbuild_dir/qemu-$VERSION
+    cp -r $qemu_src_dir $debbuild_dir/qemu-$VERSION
+    mkdir -p $debbuild_dir/qemu-$VERSION/DEBIAN
+    touch control
+
+#creating control file for debian build.
+    (cd ${scripts_dir}; ./mkcontrol.sh $VERSION > control)
+    mv $scripts_dir/control $debbuild_dir/qemu-$VERSION/DEBIAN/control
+
+#building the qemu debian with control file developed.
+    dpkg-deb --build $debbuild_dir/qemu-$VERSION
+    if [ ${?} -ne 0 ] ; then
+        echo "${0}: qemu build failed"
+        exit 1
+    fi
+}
+
+if [ ! -d ${debbuild_dir} ] ; then
+    echo "creating debbuild directory"
+    mkdir -p $debbuild_dir
+fi
+
+qemu_deb_build
+latest_qemu_build=`ls -rt $debbuild_dir | tail -1`
+cp $debbuild_dir/$latest_qemu_build build_output

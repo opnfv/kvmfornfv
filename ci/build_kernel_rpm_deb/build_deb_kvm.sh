6155fdf6915717898cacb0a341af3418b67f63ff
Swati <swatix.sharma@intel.com>
2016-06-27 17:53:53 +0530

    OPNFV KVM4NFV: Fuel-plugin scripts for deb and rpm builds creation
    These scripts will be used to build kernel-rpm and kernel-deb
    builds inside ubuntu docker (from the changed kernel code in
    kvm4nfv). After the builds are ready, they will be deployed on
    the pharos testbed to verify the created builds.
    
    Change-Id: Ie8e8108566790a6c8db2fef1c9b5799a41732b44
    
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

--- ci/build_kernel_rpm_deb/build_deb_kvm.sh
+++ ci/build_kernel_rpm_deb/build_deb_kvm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SRC=/
+SRC=/root
 CONFIG="arch/x86/configs/opnfv.config"
 VERSION="1.0.OPNFV"
-OVS_COMMIT="4ff6642f3c1dd8949c2f42b3310ee2523ee970a6"
+#OVS_COMMIT="4ff6642f3c1dd8949c2f42b3310ee2523ee970a6"
 
 quirks() {
 #
@@ -21,13 +21,6 @@ done
 
 apt-get update
 apt-get install -y git fakeroot build-essential ncurses-dev xz-utils kernel-package bc autoconf automake libtool python python-pip
-
-cd $SRC
-
-# Get the Open VSwitch sources
-rm -rf ovs
-git clone https://github.com/openvswitch/ovs.git
-cd ovs; git checkout $OVS_COMMIT
 
 cd $SRC/kvmfornfv/
 quirks kernel
@@ -50,21 +43,7 @@ make-kpkg clean
 
 fakeroot make-kpkg --initrd --revision=$VERSION kernel_image kernel_headers
 
-# Build OVS kernel modules
-cd ../../ovs
-
-quirks ovs
-pip install six
-
-./boot.sh
-./configure --with-linux=$SRC/kvmfornfv/kernel
 make
 
-# Add OVS kernel modules to kernel deb
-dpkg-deb -x $SRC/kvmfornfv/linux-image*.deb ovs.$$
-dpkg-deb --control $SRC/kvmfornfv/linux-image*.deb ovs.$$/DEBIAN
-cp datapath/linux/*.ko ovs.$$/lib/modules/*/kernel/net/openvswitch
-depmod -b ovs.$$ -a `ls ovs.$$/lib/modules`
-dpkg-deb -b ovs.$$ $SRC/kvmfornfv/linux-image*.deb
-rm -rf ovs.$$
-
+mkdir -p /root/debbuild/DEBS
+mv /root/kvmfornfv/linux-* /root/debbuild/DEBS/

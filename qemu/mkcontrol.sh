63734cf42d41086263847c3db5f8985c09b40dca
Swati Sharma <swatix.sharma@intel.com>
2016-06-29 02:34:43 +0900

    Creation of QEMU(rpm and debian builds) and Kernel(debian build)
    This includes the scripts that are added to create qemu-rpm
    and qemu-debian builds for KVM4NFV.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

---
+++ qemu/mkcontrol.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Package: qemu"
+echo "Version: $1"
+echo "Section: base"
+echo "Priority: optional"
+echo "Architecture: all"
+echo "Maintainer: Intel"
+echo "Description: control file for qemu debian build on centos"

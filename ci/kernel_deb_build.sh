f738c6a916debfe6d377da3aedf2b9b0afc521e2
Swati Sharma <swatix.sharma@intel.com>
2016-06-29 02:34:43 +0900

    Creation of QEMU(rpm and debian builds) and Kernel(debian build)
    This includes the scripts that are added to create qemu-rpm
    and qemu-debian builds for KVM4NFV.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

---
+++ ci/kernel_deb_build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+kernel_src_dir=kernel
+debbuild_tmp='/tmp'
+debbuild_dir=$debbuild_tmp/kvmfornfv_debbuild.$$
+echo $debbuild_dir
+mkdir -p $debbuild_dir
+artifact_debs=$debbuild_dir/DEBS
+mkdir -p $artifact_debs
+artifact_dir=$artifact_debs/x86_64
+mkdir -p $artifact_dir
+config_file="${kernel_src_dir}/arch/x86/configs/opnfv.config"
+output_dir="$1"
+
+usage () {
+    echo "usage: ${0} output_dir"
+    exit 1
+}
+
+if [[ -z "$@" ]]; then
+    usage
+fi
+
+if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+    echo "${0}: Output directory '${output_dir}' does not exist or cannot be written"
+    exit 1
+fi
+
+if [ ! -d ${kernel_src_dir} ] ; then
+    echo "${0}: Directory '${kernel_src_dir}' does not exist, run this script from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+if [ ! -f ${config_file} ] ; then
+    echo "${0}: ${config_file} does not exist"
+    exit 1
+fi
+
+echo
+echo "Build"
+echo
+
+cp -f ${config_file} "${kernel_src_dir}/.config"
+
+# Make timestamp part of version string for automated kernel boot verification
+date "+-%y%m%d%H%M" > "${kernel_src_dir}/localversion-zzz"
+
+( cd ${kernel_src_dir}; make DEBOPTS="--define '_topdir ${debbuild_dir}'" deb-pkg )
+if [ ${?} -ne 0 ] ; then
+    echo "${0}: Kernel build failed"
+    rm -rf ${debbuild_dir}
+    exit 1
+fi
+
+cp -f ${artifact_dir}/* ${output_dir}
+
+rm -rf ${debbuild_dir}
---
+++ ci/qemu_deb_build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+qemu_src_dir=qemu
+workspace=/root
+debbuild_dir=$workspace/debbuild
+echo $debbuild_dir
+output_dir="$1"
+
+usage () {
+    echo "usage: ${0} output_dir"
+    exit 1
+}
+
+if [[ -z "$@" ]]; then
+    usage
+fi
+
+if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+    echo "${0}: Output directory '${output_dir}' does not exist or cannot be written"
+    exit 1
+fi
+
+if [ ! -d ${qemu_src_dir} ] ; then
+    echo "${0}: Directory '${qemu_src_dir}' does not exist, run this script from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+
+echo
+echo "Build"
+echo
+
+
+debianbuild() {
+    sh ${qemu_src_dir}/qemu_build.sh ${qemu_src_dir} ${debbuild_dir}
+    if [ ${?} -ne 0 ] ; then
+        echo "${0}: qemu build failed"
+        exit 1
+    fi
+}
+
+if [ ! -d ${debbuild_dir} ] ; then
+    echo "creating debbuild directory"
+    sudo mkdir -p $debbuild_dir
+    debianbuild
+fi
+debianbuild
+latest_qemu_build=`sudo ls -rt $debbuild_dir | tail -1`
+sudo cp /root/debbuild/$latest_qemu_build build_output

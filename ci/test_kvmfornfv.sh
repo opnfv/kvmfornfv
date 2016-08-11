58c86ef732befe81438e6e806204947dd0b55d51
swatisharma <swatix.sharma@intel.com>
2016-08-11 22:46:49 +0530

    README file for CI integration for KVMforNFV.
    This patch includes the README file to explain creation of Kernel/Qemu
    builds for KVM4NFV and running cyclictest through yardstick.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -9,4 +9,44 @@
 ## and verifies the test results.
 ############################################################
 
-echo "Hello world"
+volume=/tmp/kvmtest-*
+echo $volume
+if [ -d ${volume} ] ; then
+    echo "Directory '${volume}' exists"
+    sudo rm -rf ${volume}
+fi
+
+echo ${WORKSPACE}
+time_stamp=$(date +%Y%m%d%H%M%S)
+mkdir -p /tmp/kvmtest-${time_stamp}/{image,rpm,scripts}
+echo $volume
+
+#copying required files to run yardstick cyclic testcase
+mv $WORKSPACE/build_output/* $volume/rpm
+cp -r $WORKSPACE/ci/envs/* $volume/scripts
+ls -al $volume/scripts
+cp -r $WORKSPACE/tests/cyclictest-node-context.yaml $volume
+cp -r $WORKSPACE/tests/pod.yaml $volume
+
+#Launching ubuntu docker container to run yardstick
+sudo docker run -d -i -v $volume:/opt --net=host --name kvmfornfv1 \
+kvmfornfv:latest  /bin/bash -c "cd /opt/scripts && ls;sh cyclictest.sh"
+container_id=`sudo docker ps -a | grep kvmfornfv1 |awk '{print $1}'`
+sudo docker rm $container_id
+sudo rm -rf /tmp/kvmtest-*
+
+#Verifying the results of cyclictest
+result=`grep -o '"errors":[^,]*' $volume/yardstick.out | awk -F '"' \
+'{print $4}'| awk '{if (NF=0) print "SUCCESS" }'`
+if [ "$result" = "SUCCESS" ]; then
+    echo "####################################################"
+    echo ""
+    echo `grep -o '"data":[^}]*' $volume/yardstick.out | awk -F '{' '{print $2}'`
+    echo ""
+    echo "####################################################"
+    exit 0
+else
+    echo "Testcase failed"
+    echo `grep -o '"errors":[^,]*' $volume/yardstick.out | awk -F '"' '{print $4}'`
+    exit 1
+fi

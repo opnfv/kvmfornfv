1ed2efc9d3fc6cc7efced89c85f38dbc150c270a
swatisharma <swatix.sharma@intel.com>
2016-08-18 01:02:53 +0530

    CI integration for KVMforNFV.
    This patch contains scripts for running cyclictest through yardstick
    as part of CI integration for KVMforNFV. The scripts will be
    triggered to create a docker ubuntu container for running yardstick,
    configuring the host, launching a guest VM and executing cyclictest
    through yardstick. The verification process gets completed after
    running the cyclictest.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -9,4 +9,46 @@
 ## and verifies the test results.
 ############################################################
 
-echo "Hello world"
+
+function env_clean {
+    container_id=`sudo docker ps -a | grep kvmfornfv |awk '{print $1}'`
+    sudo docker rm $container_id
+    ssh 10.2.117.23 "rm -rf /root/workspace/"
+    ssh 10.2.117.23 "pid=\$(ps aux | grep 'qemu' | awk '{print \$2}' | head -1); echo \$pid |xargs kill"
+    sudo rm -rf /tmp/kvmtest-*
+}
+
+#Cleaning up the test environment before running cyclictest through yardstick.
+env_clean
+
+time_stamp=$(date +%Y%m%d%H%M%S)
+volume=/tmp/kvmtest-${time_stamp}
+mkdir -p $volume/{image,rpm,scripts}
+
+#copying required files to run yardstick cyclic testcase
+mv $WORKSPACE/build_output/kernel-4.4*.rpm $volume/rpm
+cp -r $WORKSPACE/ci/envs/* $volume/scripts
+cp -r $WORKSPACE/tests/cyclictest-node-context.yaml $volume
+cp -r $WORKSPACE/tests/pod.yaml $volume
+
+#Launching ubuntu docker container to run yardstick
+sudo docker run -i -v $volume:/opt --net=host --name kvmfornfv \
+kvmfornfv:latest  /bin/bash -c "cd /opt/scripts && ls; ./cyclictest.sh"
+
+#Verifying the results of cyclictest
+result=`grep -o '"errors":[^,]*' $volume/yardstick.out | awk -F '"' '{print $4}'`
+
+if [ -z "$result" ]; then
+    echo "####################################################"
+    echo ""
+    echo `grep -o '"data":[^}]*' $volume/yardstick.out | awk -F '{' '{print $2}'`
+    echo ""
+    echo "####################################################"
+    env_clean
+    exit 0
+else
+    echo "Testcase failed"
+    echo `grep -o '"errors":[^,]*' ${volume}/yardstick.out | awk -F '"' '{print $4}'`
+    env_clean
+    exit 1
+fi

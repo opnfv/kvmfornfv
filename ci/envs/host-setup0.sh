cb6c786934ba094f532fa55e5d4c3968ff37f9d9
swatisharma <swatix.sharma@intel.com>
2016-08-18 01:02:53 +0530

    CI integration for KVMforNFV.
    This patch contains scripts for running cyclictest through yardstick
    as part of CI integration for KVMforNFV. The scripts will be
    triggered to create a docker ubuntu container for running yardstick,
    configuring the host, launching a guest VM and executing cyclictest
    through yardstick. The verification process gets completed after
    running the cyclictest.
    
    Co-Authored-By: Gundarapu Reddy <reddyx.gundarapu@intel.com>
    Signed-off-by: Swati Sharma <swatix.sharma@intel.com>

--- ci/envs/host-setup0.sh
+++ ci/envs/host-setup0.sh
@@ -12,7 +12,7 @@
 source host-config
 
 rpmdir=${1:-"/root/workspace/rpm/"}
-rpmpat="kernel-4.1*.rpm"
+rpmpat="kernel-4.4*.rpm"
 
 config_grub () {
     key=$1
@@ -40,7 +40,7 @@ install_kernel () {
     	exit 1
     else
     	krpm=`find "${rpmdir}" -name "${rpmpat}"`
-    	rpm -ihv $krpm
+    	#rpm -ihv $krpm
     fi
 }
 

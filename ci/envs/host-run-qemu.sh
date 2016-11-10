8c58d294fb3a6b374579680ef3788fadb78269f9
shravani <shravani.p@tcs.com>
2016-11-10 11:17:52 +0530

    This patch contains scripts to update the host_isolcpus parameter to Numa node1 CPU's,binds the qemu process threads to the host_isolcpu processors and assigns the remaining isolcpus to run stress tool
    Change-Id: I894fd18666c1e9c041776c346b7b95abce808792
    Signed-off-by: Shravani <shravani.p@tcs.com>

--- ci/envs/host-run-qemu.sh
+++ ci/envs/host-run-qemu.sh
@@ -19,7 +19,7 @@ cpumask () {
     printf 0x%x ${m}
 }
 
-qmp_sock="/tmp/qmp-sock-$$"
+qmp_sock="/tmp/qmp-sock"
 
 #${qemu} -smp ${guest_cpus} -monitor unix:${qmp_sock},server,nowait -daemonize \
 #    -cpu host,migratable=off,+invtsc,+tsc-deadline,pmu=off \
@@ -30,25 +30,20 @@ qmp_sock="/tmp/qmp-sock-$$"
 #    -device virtio-net-pci,netdev=guest0 \
 #    -nographic -serial /dev/null -parallel /dev/null
 
-${qemu} -smp ${guest_cpus} -drive file=/root/guest1.qcow2 -daemonize \
+${qemu} -smp ${guest_cpus} -monitor unix:${qmp_sock},server,nowait \
+     -drive file=/root/guest1.qcow2 -daemonize \
      -netdev user,id=net0,hostfwd=tcp:$HOST_IP:5555-:22 \
      -realtime mlock=on -mem-prealloc -enable-kvm -m 1G \
      -mem-path /mnt/hugetlbfs-1g \
      -device virtio-net-pci,netdev=net0 \
-
-i=0
-for c in `echo ${host_isolcpus} | sed 's/,/ /g'` ; do
-    cpu[$i]=${c}
-    i=`expr $i + 1`
-done
 
 threads=`echo "info cpus" | nc -U ${qmp_sock} | grep thread_id | cut -d= -f3`
 
 # Bind QEMU processor threads to RT CPUs
 i=0
 for tid in ${threads} ; do
-    tid=`printf %d ${tid}`  # this is required to get rid of cr at end
-    mask=`cpumask ${cpu[$i]}`
-    taskset -p ${mask} ${tid}
+    new_tid=`echo $tid | sed -e 's/[\r\n]//g'` # this is required to get rid of cr at end
+    mask=`cpumask ${qemu_cpu[$i]}`
+    taskset -p ${mask} ${new_tid}
     i=`expr $i + 1`
 done

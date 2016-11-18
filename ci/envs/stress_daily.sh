f7fec06cfde76dc6b5b0f565c8904f6334cbc99b
shravani <shravani.p@tcs.com>
2016-11-18 11:12:01 +0530

    This patch contains scripts for applying stress on host or guest at run time based on the stress type(cpu/memory/io).
    Change-Id: I845b3ec028f375cf0809fb3a184b1511787d263b
    Signed-off-by: Shravani <shravani.p@tcs.com>

---
+++ ci/envs/stress_daily.sh
@@ -0,0 +1,26 @@
+##########################################################################
+#This script will pass arguments to stress script based on the stress type
+##########################################################################
+#!/bin/bash
+source host-config
+
+stress_type=$1
+timeout=10m
+
+case $stress_type in
+   cpu) # processor
+      ARGS="--cpu=100"
+      ;;
+   memory)
+      ARGS="--vm=100"
+      ;;
+   io)
+      ARGS="--io 10 --hdd 100"
+      ;;
+   *)
+      echo $"Usage: $0 {cpu|memory|io}"
+      exit 1
+esac
+
+#stress_isolcpus will hold range as a value i.e, eg :24-43
+sh stress_scripts.sh -c ${stress_isolcpus} -t $timeout -a $ARGS

87ee23b29af38113521b33ab1824d6d229631cc6
shravani <shravani.p@tcs.com>
2016-11-23 19:28:52 +0530

    This patch updates the interface scripts to trigger packet forwarding and stress scripts for daily Job.
    Change-Id: Ib76df1fa9fde0a77764b05e4fea29338243e05c2
    Signed-off-by: Shravani <shravani.p@tcs.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -8,38 +8,101 @@
 ## verify/daily as an argument
 ############################################################
 
-test_type=$1
+#!/bin/bash
 
+############################################################
+## This script  is an interface to trigger the
+## cyclicTestTrigger.sh for test type like patch verification,
+## daily testing.
+## Releng will trigger this script by passing test type as
+## verify/daily and test name as idle_idle/stress_idle/
+## packet_forward as arguments.
+## Verify Job runs idle_idle,packet_forward test
+## daily job runs base on the test name parameter
+############################################################
+
+test_type=$1
+test_name=$2
+cyclictest_env=("idle_idle" "stress_idle") #cyclictest environment
+cyclictest_result=0 #exit code of cyclictest
+packetforward_result=0 #exit code of packet forward
+
+function packetForward {
+   source $WORKSPACE/ci/packet_forward_test.sh $HOST_IP
+   packetforward_result=$?
+   if [ ${packetforward_result} -ne 0 ];then
+      echo "Packet Forwarding test case execution FAILED"
+   else
+      echo "Packet Forwarding test case executed SUCCESSFULLY"
+   fi
+   host_clean
+}
+
+function cyclictest {
+   test_case=$1
+   source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type $test_case
+   #calculating and verifying sha512sum of the guestimage.
+   if ! verifyGuestImage;then
+      exit 1
+   fi
+   #Update kvmfornfv_cyclictest_${testName}.yaml with test_time and pod.yaml with IP
+   updateYaml
+   #Cleaning up the test environment before running cyclictest through yardstick.
+   env_clean
+   #Creating a docker image with yardstick installed and launching ubuntu docker to run yardstick cyclic testcase
+   if runCyclicTest;then
+      cyclictest_result=`expr ${cyclictest_result} + 0`
+   else
+      echo "Test case execution FAILED for ${test_case} environment"
+      cyclictest_result=`expr ${cyclictest_result} + 1`
+   fi
+}
+
+#Execution of testcases based on test type and test name from releng.
 if [ ${test_type} == "verify" ];then
    HOST_IP="10.2.117.23"
    test_time=600000 # 10m
+   for env in ${cyclictest_env[@]}
+   do
+      #Executing cyclictest through yardstick.
+      cyclictest ${env}
+      sleep 5
+   done 
+   #Execution of packet forwarding test cases.
+   packetForward
+   if [ ${cyclictest_result} -ne 0 ] ||  [ ${packetforward_result} -ne 0 ];then
+      echo "Test case FAILED"
+      exit 1
+   else
+      exit 0
+   fi
 elif [ ${test_type} == "daily" ];then
    HOST_IP="10.2.117.25"
-   test_time=7200000 #2h
+   test_time=3600000 #1h
+   if [ ${test_name} == "packet_forward" ];then
+      packetForward
+      if [ ${packetforward_result} -ne 0 ] ; then
+         exit 1
+      else
+         exit 0
+      fi
+   elif [ ${test_name} == "Cyclictest" ];then
+      for env in ${cyclictest_env[@]}
+      do
+         #Executing cyclictest through yardstick.
+         cyclictest ${env}
+         sleep 5
+      done
+      if [ ${cyclictest_result} -ne 0 ] ; then
+         echo "Cyclictest case execution FAILED"
+         exit 1
+      else
+         echo "Cyclictest case executed SUCCESSFULLY"
+         exit 0
+      fi
+   fi
 elif [ ${test_type} == "merge" ];then
    echo "Test is not enabled for ${test_type}"
    exit 0
 else
    echo "Incorrect test type ${test_type}"
-   exit 1
-fi
-
-source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type
-
-#calculating and verifying sha512sum of the guestimage.
-if ! verifyGuestImage;then
-   exit 1
-fi
-
-#Update kvmfornfv_cyclictest_idle_idle.yaml with test_time and pod.yaml with IP
-updateYaml
-
-#Cleaning up the test environment before running cyclictest through yardstick.
-env_clean
-
-#Creating a docker image with yardstick installed and launching ubuntu docker to run yardstick cyclic testcase
-if runCyclicTest;then
-   exit 0
-else
-   exit 1
-fi

272ba58aa158ae759dec04cadc8af5230027c8f4
kalyanreddy <reddyx.gundarapu@intel.com>
2016-12-14 16:08:49 +0530

    This patch includes updated scripts for optimizing the code for executing all the test cases and performing host cleaning operations after executing all the test cases.
    Change-Id: I3460235e7d9c95090c9523507e6c5e4d43f9c410
    Co-Authored-By: Y Rajitha <y.rajitha@tcs.com>
    Signed-off-by: Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>

--- ci/cyclicTestTrigger.sh
+++ ci/cyclicTestTrigger.sh
@@ -99,47 +99,51 @@ function cleanup {
 
 #Creating a docker image with yardstick installed and Verify the results of cyclictest
 function runCyclicTest {
-   docker_image_dir=$WORKSPACE/docker_image_build
-   ( cd ${docker_image_dir}; sudo docker build  -t kvmfornfv:latest --no-cache=true . )
-   if [ ${?} -ne 0 ] ; then
-      echo  "Docker image build failed"
-      id=$(sudo docker ps -a  | head  -2 | tail -1 | awk '{print $1}'); sudo docker rm -f $id
-      exit 1
+   container_id=`sudo docker ps -a | grep kvmfornfv_${testType} |awk '{print \$1}'|sed -e 's/\r//g'`
+   if [ -z "$container_id" ]; then
+      docker_image_dir=$WORKSPACE/docker_image_build
+      ( cd ${docker_image_dir}; sudo docker build  -t kvmfornfv:latest --no-cache=true . )
+      if [ ${?} -ne 0 ] ; then
+         echo  "Docker image build failed"
+         id=$(sudo docker ps -a  | head  -2 | tail -1 | awk '{print $1}'); sudo docker rm -f $id
+         exit 1
+      fi
+      time_stamp=$(date +%Y%m%d%H%M%S)
+      volume=/tmp/kvmtest-${testType}-${time_stamp}
+      mkdir -p $volume/{image,rpm,scripts}
+
+      #copying required files to run yardstick cyclic testcase
+      cp $WORKSPACE/build_output/kernel-${KERNELRPM_VERSION}*.rpm ${volume}/rpm
+      cp -r $WORKSPACE/ci/envs/* ${volume}/scripts
+      cp -r $WORKSPACE/tests/kvmfornfv_cyclictest_${testName}.yaml ${volume}
+      cp -r $WORKSPACE/tests/pod.yaml ${volume}/scripts
+
+      #Launching ubuntu docker container to run yardstick
+      sudo docker run -i -v ${volume}:/opt --net=host --name kvmfornfv_${testType} \
+      kvmfornfv:latest  /bin/bash -c "cd /opt/scripts && ls; ./cyclictest.sh $testType $testName"
+      cyclictest_output=$?
+   else
+      cp -r $WORKSPACE/tests/kvmfornfv_cyclictest_${testName}.yaml ${volume}
+      sudo docker start $container_id
+      sudo docker exec $container_id /bin/bash -c "cd /opt/scripts && ls; ./cyclictest.sh $testType $testName"
+      cyclictest_output=$?
    fi
-   time_stamp=$(date +%Y%m%d%H%M%S)
-   volume=/tmp/kvmtest-${testType}-${time_stamp}
-   mkdir -p $volume/{image,rpm,scripts}
-
-   #copying required files to run yardstick cyclic testcase
-   cp $WORKSPACE/build_output/kernel-${KERNELRPM_VERSION}*.rpm ${volume}/rpm
-   cp -r $WORKSPACE/ci/envs/* ${volume}/scripts
-   cp -r $WORKSPACE/tests/kvmfornfv_cyclictest_${testName}.yaml ${volume}
-   cp -r $WORKSPACE/tests/pod.yaml ${volume}/scripts
-
-   #Launching ubuntu docker container to run yardstick
-   sudo docker run -i -v ${volume}:/opt --net=host --name kvmfornfv_${testType}_${testName} \
-   kvmfornfv:latest  /bin/bash -c "cd /opt/scripts && ls; ./cyclictest.sh $testType $testName"
-   cyclictest_output=$?
    #Verifying the results of cyclictest
 
    if [ "$testType" == "verify" ];then
       result=`grep -o '"errors":[^,]*' ${volume}/yardstick.out | awk -F '"' '{print $4}'`
 
-      if [ -z "${result}" ]; then
+      if [ -z "${result}" ] && [ "${cyclictest_output}" == 0 ]; then
          echo "####################################################"
          echo ""
          echo `grep -o '"data":[^}]*' ${volume}/yardstick.out | awk -F '{' '{print $2}'`
          echo ""
          echo "####################################################"
-         cleanup $cyclictest_output
+         return 0
       else
          echo "Testcase failed"
          echo `grep -o '"errors":[^,]*' ${volume}/yardstick.out | awk -F '"' '{print $4}'`
-         env_clean
-         host_clean
          return 1
       fi
-   else
-      cleanup $cyclictest_output
    fi
 }

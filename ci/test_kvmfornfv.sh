335d3574ca86ed2b9a5ee3c555200404c761d708
kalyanreddy <reddyx.gundarapu@intel.com>
2016-12-14 14:31:06 +0530

    This patch includes updated scripts for optimizing the code for executing all the test cases and performing host cleaning operations after executing all the test cases.
    Change-Id: I3460235e7d9c95090c9523507e6c5e4d43f9c410
    Co-Authored-By: Y Rajitha <y.rajitha@tcs.com>
    Signed-off-by: Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -27,20 +27,19 @@ function packetForward {
    else
       echo "Packet Forwarding test case executed SUCCESSFULLY"
    fi
+   env_clean
    host_clean
 }
 
 function cyclictest {
    test_case=$1
    source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type $test_case
-   #calculating and verifying sha512sum of the guestimage.
-   if ! verifyGuestImage;then
-      exit 1
-   fi
    #Update kvmfornfv_cyclictest_${testName}.yaml with test_time and pod.yaml with IP
    updateYaml
    #Cleaning up the test environment before running cyclictest through yardstick.
+   if [ ${test_case} == "idle_idle" ];then
    env_clean
+   fi
    #Creating a docker image with yardstick installed and launching ubuntu docker to run yardstick cyclic testcase
    if runCyclicTest;then
       cyclictest_result=`expr ${cyclictest_result} + 0`
@@ -50,10 +49,27 @@ function cyclictest {
    fi
 }
 
+function dockerImageBuild {
+   docker_image_dir=$WORKSPACE/docker_image_build
+   ( cd ${docker_image_dir}; sudo docker build  -t kvmfornfv:latest --no-cache=true . )
+   if [ ${?} -ne 0 ] ; then
+      echo  "Docker image build failed"
+      id=$(sudo docker ps -a  | head  -2 | tail -1 | awk '{print $1}'); sudo docker rm -f $id
+      exit 1
+   fi
+}
+
+
 #Execution of testcases based on test type and test name from releng.
 if [ ${test_type} == "verify" ];then
    HOST_IP="10.2.117.23"
    test_time=120000 # 2m
+   source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type
+   #calculating and verifying sha512sum of the guestimage.
+   if ! verifyGuestImage;then
+      exit 1
+   fi
+   dockerImageBuild
    for env in ${cyclictest_env_verify[@]}
    do
       #Executing cyclictest through yardstick.
@@ -79,6 +95,12 @@ elif [ ${test_type} == "daily" ];then
          exit 0
       fi
    elif [ ${test_name} == "cyclictest" ];then
+      source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type
+      #calculating and verifying sha512sum of the guestimage.
+      if ! verifyGuestImage;then
+         exit 1
+      fi
+      dockerImageBuild
       for env in ${cyclictest_env_daily[@]}
       do
          #Executing cyclictest through yardstick.
@@ -92,6 +114,8 @@ elif [ ${test_type} == "daily" ];then
          echo "Cyclictest case executed SUCCESSFULLY"
          exit 0
       fi
+      env_clean
+      host_clean
    fi
 elif [ ${test_type} == "merge" ];then
    echo "Test is not enabled for ${test_type}"

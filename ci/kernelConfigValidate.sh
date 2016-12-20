ca76fbcfa2d801b980eb237e246938a953c3a246
shravani <shravani.p@tcs.com>
2016-12-20 15:09:34 +0530

    Combine the common code for kernel building on deb/rpm
    This patch removes the duplication of code in rpm/debian build
    scripts.
    
    Change-Id: I88edc93df488bd01a43cd98e4f69f35e09f8f61c
    Signed-off-by: Shravani <shravani.p@tcs.com>

---
+++ ci/kernelConfigValidate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+kernel_src_dir=kernel
+kernel_config_file="${kernel_src_dir}/arch/x86/configs/opnfv.config"
+
+function show_stage {
+    echo
+    echo $1
+    echo
+}
+
+function kernel_build_validate {
+    show_stage "validate"
+    if [[ -z "$@" ]]; then
+        echo "usage: ${0} output_dir"
+        echo "usage: ${1} pkg_type"
+        usage
+    fi
+    output_dir="$1"
+    pkg_type="$2"
+    if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+        echo "${0}: Output directory '${output_dir}' does not exist or cannot be written"
+        exit 1
+    fi
+    if [ ! -d ${kernel_src_dir} ] ; then
+        echo "${0}: Directory '${kernel_src_dir}' does not exist, run this script from the root of kvmfornfv source tree"
+        exit 1
+    fi
+
+    if [ ! -f ${kernel_config_file} ] ; then
+        echo "${0}: ${kernel_config_file} does not exist"
+        exit 1
+    fi
+    echo
+    echo "Build"
+    echo
+}
+
+function kernel_build_prep {
+    show_stage "kernel tree prep"
+    cp -f ${kernel_config_file} "${kernel_src_dir}/.config"
+    make olddefconfig
+}

c95be7718e4b7303a065e7213f09d4179ab6c14e
Shravani <shravani.p@tcs.com>
2017-02-14 16:41:10 +0530

    This patch contains the PCM utility installation and continously collecting memory bandwidth data using pcm-memory throughout each cyclic testcase
    Change-Id: I63427b6b700f1e3dcfaf95627de7f4db8b8dc300
    Co-Authored by:Rajitha <y.rajitha@tcs.com>
    Signed-off-by:Shravani <shravani.p@tcs.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -41,6 +41,8 @@ function cyclictest {
    updateYaml
    #Cleaning up the test environment before running cyclictest through yardstick.
    env_clean
+   #Running PCM utility
+   collect_MBWInfo $test_type 
    #Creating a docker image with yardstick installed and launching ubuntu docker to run yardstick cyclic testcase
    if runCyclicTest;then
       cyclictest_result=`expr ${cyclictest_result} + 0`
@@ -48,12 +50,47 @@ function cyclictest {
       echo "Test case execution FAILED for ${test_case} environment"
       cyclictest_result=`expr ${cyclictest_result} + 1`
    fi
+   echo "Terminating PCM Process"
+   sudo ssh root@${HOST_IP} "pid=\$(ps aux | grep 'pcm' | awk '{print \$2}' | head -1); echo \$pid |xargs kill -SIGTERM"
+}
+function collect_MBWInfo {
+   #Collecting the Memory Bandwidth Information using pcm-memory utility
+   source $WORKSPACE/ci/envs/host-config
+   testType=$1
+   timeStamp=$(date +%Y%m%d%H%M%S)
+   echo "Running PCM memory to collect memory bandwidth"
+   sudo ssh root@${HOST_IP} "mkdir -p /root/MBWInfo"
+   sudo ssh root@${HOST_IP} "${pcm_memory} 60 &>/root/MBWInfo/MBWInfo_${testType}_${timeStamp} &disown"
+}
+function install_pcm {
+   source $WORKSPACE/ci/envs/host-config
+   sudo ssh root@${HOST_IP} '
+   modelName=`cat /proc/cpuinfo | grep -i "model name" | uniq`
+   if  echo "$modelName" | grep -i "xeon" ;then
+   echo  "pcm utility supports $modelName processor"
+   else
+   echo "check for the pcm utility supported processors"
+   exit 1
+   fi
+   cd /root
+   if [ ! -d "pcm" ]; then
+    `git clone https://github.com/opcm/pcm` 
+     cd pcm
+     make
+   fi
+   echo "Disabling NMI Watchdog"  
+   echo 0 > /proc/sys/kernel/nmi_watchdog
+   echo "To Access MSR registers installing msr-tools"
+   sudo yum install msr-tools
+   sudo modprobe msr
+   '
 }
 
 #Execution of testcases based on test type and test name from releng.
 if [ ${test_type} == "verify" ];then
    HOST_IP="10.10.100.21"
    test_time=120000 # 2m
+   install_pcm
    for env in ${cyclictest_env_verify[@]}
    do
       #Executing cyclictest through yardstick.
@@ -71,6 +108,7 @@ if [ ${test_type} == "verify" ];then
 elif [ ${test_type} == "daily" ];then
    HOST_IP="10.10.100.22"
    test_time=3600000 #1h
+   install_pcm
    if [ ${test_name} == "packet_forward" ];then
       packetForward
       if [ ${packetforward_result} -ne 0 ] ; then
@@ -80,7 +118,7 @@ elif [ ${test_type} == "daily" ];then
       fi
    elif [ ${test_name} == "cyclictest" ];then
       for env in ${cyclictest_env_daily[@]}
-      do
+      do 
          #Executing cyclictest through yardstick.
          cyclictest ${env}
          sleep 5

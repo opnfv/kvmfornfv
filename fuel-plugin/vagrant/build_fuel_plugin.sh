cf6406319d56bb2d97c78c42ea2d2f10b154a78d
Yunhong Jiang <yunhong.jiang@linux.intel.com>
2017-02-27 15:52:54 -0800

    Match the config from ubuntu
    As ubuntu 16.04 is using the same version kernel, we try to reuse the
    config file from ubuntu server release.
    
    Also added one rpm package required for building the new kernel config.
    
    upstream-status: N/A
    
    Change-Id: I471918eb1840bd36ba569abb63b2fa0b3d2fb4f3
    Signed-off-by: Yunhong Jiang <yunhong.jiang@linux.intel.com>

--- fuel-plugin/vagrant/build_fuel_plugin.sh
+++ fuel-plugin/vagrant/build_fuel_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y ruby-dev rubygems-integration python-pip rpm createrepo dpkg-dev
+sudo apt-get install -y ruby-dev rubygems-integration python-pip rpm createrepo dpkg-dev libssl-dev
 sudo gem install fpm
 sudo pip install fuel-plugin-builder
 sudo apt-get install docker.io -y

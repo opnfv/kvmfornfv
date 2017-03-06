d30b50d00b95bda573e9fdced45f1fb94e58b31b
RajithaY <rajithax.yerrumsetty@intel.com>
2017-03-06 18:48:31 +0530

    This patch includes building of the kernel rpm's for the commit id provided in apex.conf file and renaming of the kernel rpm's in the format kvmfornfv-commitid-apex-kernel-[devel/header]-4.4.6_rt14_centos.x86_64.rpm
    Change-Id: Id36eec6a577ac92b436a68f975fc1293c04ab97d
    Co-Authored by: Navya Bathula <navyax.bathula@intel.com>
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

---
+++ ci/apex_build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function checkout_commit() {
+build_dir=/opt/kvmfornfv/
+mkdir -p /tmp/kvm
+SRC=/tmp/kvm
+source ${build_dir}/ci/apex.conf
+cd $SRC
+git clone $build_dir $SRC
+cd kvmfornfv
+git checkout -f ${commit_id}
+mkdir ${output_dir}
+}
+
+output_dir="$1"
+checkout_commit
+
+kernel_src_dir=$SRC/kvmfornfv/kernel
+rpmbuild_dir=$SRC/kvmfornfv_rpmbuild.$$
+artifact_dir=${rpmbuild_dir}/RPMS/x86_64
+mkdir -p $artifact_dir
+config_file="${kernel_src_dir}/arch/x86/configs/opnfv.config"
+
+usage () {
+    echo "usage: ${0} output_dir"
+    exit 1
+}
+
+
+if [[ -z "$@" ]]; then
+    usage
+fi
+
+if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+    echo "${0}: Output directory '${output_dir}' does not exist or cannot \
+          be written"
+    exit 1
+fi
+
+if [ ! -d ${kernel_src_dir} ] ; then
+    echo "${0}: Directory '${kernel_src_dir}' does not exist, run this script \
+          from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+if [ ! -f ${config_file} ] ; then
+    echo "${0}: ${config_file} does not exist"
+    exit 1
+fi
+
+echo
+echo "Build"
+echo
+
+cp -f ${config_file} "${kernel_src_dir}/.config"
+
+# Make timestamp part of version string for automated kernel boot verification
+date "+-%y%m%d%H%M" > "${kernel_src_dir}/localversion-zzz"
+
+(cd ${kernel_src_dir}; make RPMOPTS="--define '_topdir ${rpmbuild_dir}'" rpm-pkg)
+if [ ${?} -ne 0 ] ; then
+    echo "${0}: Kernel build failed"
+    rm -rf ${rpmbuild_dir}
+    exit 1
+fi
+
+cp -f ${artifact_dir}/* ${output_dir}
+mv ${output_dir}/* ${build_dir}/build_output/
+
+rm -rf ${rpmbuild_dir}
+#cleaning the /tmp
+rm -rf ${SRC}

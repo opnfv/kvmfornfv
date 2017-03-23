17949d53666c316216efb18d11f02700139780c9
RajithaY <rajithax.yerrumsetty@intel.com>
2017-03-23 16:10:04 +0530

    This patch includes to build the kernel rpms on danube branch
    Change-Id: I197ff3f9a51b3b34b31d0b9a3628610f7f915011
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

--- ci/apex_build.sh
+++ ci/apex_build.sh
@@ -8,8 +8,11 @@ source ${build_dir}/ci/apex.conf
 cd $SRC
 #Cloning into /tmp/kvmfornfv from local repository
 git clone $build_dir $SRC
-if [ "$branch" == "master" ] || [ "$branch" == "danube" ];then
+if [[ "$branch" == "master" ]] || [[ "$branch" == *"danube"* ]];then
    echo "Checking out on $branch branch"
+   git branch
+   git checkout $branch
+   git branch
    echo "Commit-id is ${commit_id}"
    git checkout -f ${commit_id}
    if [ $? -ne 0 ];then

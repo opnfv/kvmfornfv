f8c91fe5ae79b2ada99eca84affa881e2b273f21
RajithaY <rajithax.yerrumsetty@intel.com>
2017-03-30 16:30:49 +0530

    This patch includes the files to collect and add the memory bandwidth information through cyclictest
    Change-Id: I197831cfc6f7918da1de00c0ad72b640e1cdb5ef
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

---
+++ ci/envs/host-collect-MBWinfo.sh
@@ -0,0 +1,9 @@
+testtype=$2
+testName=$1
+echo "Running PCM memory to collect memory bandwidth"
+mkdir /root/MBWInfo
+pcm_memory=/root/pcm/pcm-memory.x
+timeStamp=$(date +%Y-%m-%d-%H-%M-%S)
+${pcm_memory} 60 &>/root/MBWInfo/MBWInfo_${testName}_${testType}_${timeStamp} &disown
+pid=$(ps aux | grep 'pcm' | awk '{print \$2}' | head -1)
+echo "PCM tool is running on the pid:$pid"

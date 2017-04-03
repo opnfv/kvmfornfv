df399a014a40efae8917233b6c6b900f2a70696f
RajithaY <rajithax.yerrumsetty@intel.com>
2017-04-03 13:44:05 +0530

    This patch includes the files to collect and add the memory bandwidth information through cyclictest and push values of read/write throughput information to influxdb
    Change-Id: Iad35e6d1ee82c98058e4280752b8fc1d0a300bb1
    Co-Authored by:Srinivas <srinivas.atmakuri@tcs.com>
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

--- ci/test_kvmfornfv.sh
+++ ci/test_kvmfornfv.sh
@@ -14,7 +14,8 @@
 test_type=$1
 test_name=$2
 ftrace_enable=0
-cyclictest_env_verify=("idle_idle" "cpustress_idle" "memorystress_idle" "iostress_idle") #cyclictest environment
+#cyclictest_env_verify=("idle_idle" "cpustress_idle" "memorystress_idle" "iostress_idle") #cyclictest environment
+cyclictest_env_verify=("iostress_idle")
 cyclictest_env_daily=("idle_idle" "cpustress_idle" "memorystress_idle" "iostress_idle")
 cyclictest_result=0 #exit code of cyclictest
 packetforward_result=0 #exit code of packet forward
@@ -53,18 +54,20 @@ function getTestParams {
 function cyclictest {
    test_case=$1
    source $WORKSPACE/ci/cyclicTestTrigger.sh $HOST_IP $test_time $test_type $test_case
-   #Verifying whether the test node is up and running
+    #Verifying whether the test node is up and running
    connect_host
    #Waiting for ssh to be available for the host machine.
    sleep 20
+   if [ ${test_case} != "idle_idle" ];then
+   echo "Checking the logs"
+   sudo ssh root@${HOST_IP} "cd /root/MBWInfo;ls -ltr MBW*"
+   fi
    #calculating and verifying sha512sum of the guestimage.
    if ! verifyGuestImage;then
       exit 1
    fi
    #Update kvmfornfv_cyclictest_${testName}.yaml with test_time and pod.yaml with IP
    updateYaml
-   #Running PCM utility
-   collect_MBWInfo $test_type
    #Cleaning the environment before running cyclictest through yardstick
    env_clean
    #Creating a docker image with yardstick installed and launching ubuntu docker to run yardstick cyclic testcase
@@ -74,18 +77,10 @@ function cyclictest {
       echo "Test case execution FAILED for ${test_case} environment"
       cyclictest_result=`expr ${cyclictest_result} + 1`
    fi
-   echo "Terminating PCM Process"
-   sudo ssh root@${HOST_IP} "pid=\$(ps aux | grep 'pcm' | awk '{print \$2}' | head -1); echo \$pid |xargs kill -SIGTERM"
 }
-#Collecting the Memory Bandwidth Information using pcm-memory utility
-function collect_MBWInfo {
-   testType=$1
-   timeStamp=$(date +%Y%m%d%H%M%S)
-   echo "Running PCM memory to collect memory bandwidth"
-   sudo ssh root@${HOST_IP} "mkdir -p /root/MBWInfo"
-   sudo ssh root@${HOST_IP} "${pcm_memory} 60 &>/root/MBWInfo/MBWInfo_${testType}_${timeStamp} &disown"
-}
+
 function install_pcm {
+   sudo ssh root@${HOST_IP} "echo "Disabling NMI watchdog";echo 0 > /proc/sys/kernel/nmi_watchdog"
    sudo ssh root@${HOST_IP} '
    modelName=`cat /proc/cpuinfo | grep -i "model name" | uniq`
    if echo "$modelName" | grep -i "xeon" ;then
@@ -127,7 +122,7 @@ if [ ${test_type} == "verify" ];then
       for env in ${cyclictest_env_verify[@]}
       do
          #Executing cyclictest through yardstick.
-         #cyclictest ${env}
+         cyclictest ${env}
          echo "Debugging in progress.Execution of cyclictest is disabled"
          sleep 10
       done

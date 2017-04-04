7ab4bd2d82eb0d8201d49d568c520784c33649a0
kalyanreddy <reddyx.gundarapu@intel.com>
2017-04-04 14:55:23 +0530

    This patch includes scripts to push test results in to local influxdb and then publish on local grafana dashboard.
    Change-Id: If983268b8e97642c2d19c74d13525137011b4fb7
    Signed-off-by: Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>

---
+++ ci/envs/data_publish.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import glob, os
+import csv
+import json
+import sys
+import ast
+import subprocess
+from influxdb import InfluxDBClient
+
+time_stamp=sys.argv[1]
+results_dir=sys.argv[2]
+test_type=sys.argv[3]
+print(time_stamp)
+print(results_dir)
+print(test_type)
+
+def publish_results(testtype):
+    for file in glob.glob("*.csv"):
+        print(file)
+        f = open( file, 'r' )
+        reader = csv.DictReader( f )
+        result = json.dumps( [ row for row in reader ] )
+        result = ast.literal_eval(result)
+        print(result)
+
+        for i in result:
+            test = i['id'] + '_' +testtype
+            json_body = [
+                 {
+                     "measurement": test,
+                     "tags": {
+                     "id": i['id'],
+                     "type": i['type'],
+                     "packet_size": i['packet_size']
+                     },
+                     "time": time_stamp,
+                     "fields": {
+                     "min_value": i['min_latency_ns'],
+                     "avg_value": i['avg_latency_ns'],
+                     "max_value": i['max_latency_ns']
+                     }
+                 }
+            ]
+            print(time_stamp)
+            client = InfluxDBClient('10.10.100.20', 8086, 'admin', 'admin', 'packet_forwarding')
+            client.switch_database('packet_forwarding')
+            client.write_points(json_body)
+
+os.chdir(results_dir)
+publish_results(test_type)

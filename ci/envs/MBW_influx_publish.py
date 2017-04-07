5f8dd8cda8bf01e2aea9e1274004ca171fdb5e87
RajithaY <rajithax.yerrumsetty@intel.com>
2017-04-07 17:28:46 +0530

    This patch includes the files to collect and add the memory bandwidth information through cyclictest and push values of read/write throughput information to influxdb
    Change-Id: Ia104bdb43de677840aac636db325be99d77df586
    Co-Authored by:Srinivas <srinivas.atmakuri@tcs.com>
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

---
+++ ci/envs/MBW_influx_publish.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import json
+import ast
+import sys
+from influxdb import InfluxDBClient
+import getopt
+import datetime
+
+def json_create():
+#Json file containing measurement,fields,tags is created
+    options, args = getopt.getopt(sys.argv[4:], 'r:w:m:')
+    read = []
+    write = []
+    memory = []
+    json_body=[]
+    time_stamp = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d-%H-%M-%S")
+    for opt, arg in options:
+        if opt in ('-w'):
+            write = arg.split("\n")
+        elif opt in ('-r'):
+            read = arg.split("\n")
+        elif opt in ('-m'):
+            memory = arg.split("\n")
+
+    for read_v,memory_v,write_v in map(None,read,memory,write):
+        json_body.append(
+            {
+                "measurement": "kvmfornfv_cyclictest_" + sys.argv[1] + "_MBW",
+                "tags": {
+                    "id": "pcm data",
+                },
+                "time": time_stamp,
+                "fields": {
+                    "read_throughput": read_v,
+                    "write_throughput": write_v,
+                    "memory_throughput": memory_v,
+                }
+            }
+        )
+        time_stamp = time_stamp + datetime.timedelta(seconds=int(sys.argv[3]))
+    return json_body
+
+def influx_push(json_body):
+# JSON is pushed into "yardstick" database of influxdb
+    client = InfluxDBClient('10.10.100.20', 8086, 'admin', 'admin', 'yardstick')
+    client.write_points(json_body)
+
+influx_push(json_body=json_create())

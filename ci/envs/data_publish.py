66b7a8e972b26760b6a087459f331636f7138e7d
RajithaY <rajithax.yerrumsetty@intel.com>
2017-04-19 18:09:09 +0530

    Integrating common code for data publish
    This patch includes the scripts to integrate the interface code for
    packet forwarding and memory bandwidth information data publish
    
    Change-Id: Ic581f95f62114bd72f53ffea111d7a510a740be3
    Co-Authored by:Srinivas <srinivas.atmakuri@tcs.com>
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

--- ci/envs/data_publish.py
+++ ci/envs/data_publish.py
@@ -1,17 +1,20 @@
 from __future__ import print_function
+import json
+import ast
+import sys
+import datetime
 import glob, os
 import csv
-import json
-import sys
-import ast
 import subprocess
 from influxdb import InfluxDBClient
 
-time_stamp=sys.argv[1]
-test_type=sys.argv[2]
-results_dir=sys.argv[3]
+def influx_push(json_body):
+# JSON is pushed into "yardstick" database of influxdb
+    client = InfluxDBClient('104.197.68.199', 8086, 'opnfv', '0pnfv2015', 'yardstick')
+    client.write_points(json_body)
 
-def publish_results(testtype):
+def csv_json_create():
+#Converts the CSV file to JSON
     for file in glob.glob("*.csv"):
         print(file)
         f = open( file, 'r' )
@@ -19,29 +22,68 @@ def publish_results(testtype):
         result = json.dumps( [ row for row in reader ] )
         result = ast.literal_eval(result)
         print(result)
+        return result
 
-        for i in result:
-            test = i['id'] + '_' +testtype
-            json_body = [
-                 {
-                     "measurement": test,
-                     "tags": {
-                     "id": i['id'],
-                     "type": i['type'],
-                     "packet_size": i['packet_size']
-                     },
-                     "time": time_stamp,
-                     "fields": {
-                     "min_value": i['min_latency_ns'],
-                     "avg_value": i['avg_latency_ns'],
-                     "max_value": i['max_latency_ns']
-                     }
-                 }
-            ]
-            print(time_stamp)
-            client = InfluxDBClient('104.197.68.199', 8086, 'opnfv', '0pnfv2015', 'yardstick')
-            client.switch_database('yardstick')
-            client.write_points(json_body)
+def packetforwarding_publish():
+#Function to publish packetforwarding logs into influxdb
+    time_stamp=sys.argv[1]
+    test_type=sys.argv[2]
+    results_dir=sys.argv[3]
+    result=csv_json_create()
+    influx_push(json_body=pf_json(result,test_type))
 
-os.chdir(results_dir)
-publish_results(test_type)
+def mbwInfo_publish():
+#Function to publish Memory Bandwidth logs into influxdb
+    test_type=sys.argv[1]
+    results_dir=sys.argv[2]
+    result=csv_json_create()
+    influx_push(json_body=mbwInfo_json(result,test_type))
+
+def pf_json(result,test_type):
+#Creates a modified JSON of packetforwarding for influxdb
+    json_body=[]
+    for i in result:
+        test = i['id'] + '_' +test_type
+        json_body.append(
+            {
+                "measurement": test,
+                "tags": {
+                "id": i['id'],
+                "type": i['type'],
+                   "packet_size": i['packet_size']
+                   },
+                "time": time_stamp,
+                "fields": {
+                   "min_value": i['min_latency_ns'],
+                   "avg_value": i['avg_latency_ns'],
+                   "max_value": i['max_latency_ns']
+                }
+            }
+        )
+    return json_body
+
+def mbwInfo_json(result,test_type):
+#Creates a modified JSON of Memory Bandwidth for influxdb
+    json_body=[]
+    for i in result:
+        json_body.append(
+            {
+                "measurement": "kvmfornfv_cyclictest_" + test_type +"_MBW",
+                "tags": {
+                    "id": "pcm data",
+                },
+                "time": i['Date']+"T"+i['Time']+"Z",
+                "fields": {
+                    "read_throughput": i['Read'],
+                    "write_throughput": i['Write'],
+                }
+            }
+        )
+    return(json_body)
+
+if (len(sys.argv) == 4):
+    packetforwarding_publish()
+else if (len(sys.argv) == 3):
+    mbwInfo_publish
+else:
+    print("Error: No suitable test case name passed")

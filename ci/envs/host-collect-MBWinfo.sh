bbf5e867b5d9ad57400091eb5e13e3f1abadec2f
RajithaY <rajithax.yerrumsetty@intel.com>
2017-04-19 18:09:09 +0530

    Integrating common code for data publish
    This patch includes the scripts to integrate the interface code for
    packet forwarding and memory bandwidth information data publish
    
    Change-Id: Ic581f95f62114bd72f53ffea111d7a510a740be3
    Co-Authored by:Srinivas <srinivas.atmakuri@tcs.com>
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

---
+++ ci/envs/host-collect-MBWinfo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This script is used to collect the memory read and write information using pcm memory tool
+testtype=$2
+testName=$1
+echo 0 > /proc/sys/kernel/nmi_watchdog
+echo "Running PCM memory to collect memory bandwidth"
+timeStamp=$(date +%Y-%m-%d-%H-%M-%S)
+mkdir -p /root/MBWInfo/MBWInfo_${testName}_${testtype}_${timeStamp}
+pcm_memory=/root/pcm/pcm-memory.x
+${pcm_memory} 30 -csv &>/root/MBWInfo/MBWInfo_${testName}_${testtype}_${timeStamp}/MBWInfo_${testName}.csv &disown
+pid=$(ps aux | grep 'pcm' | awk '{print $2}' | head -1)
+echo "PCM tool is running on the pid:$pid"

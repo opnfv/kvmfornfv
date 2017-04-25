3c5f2de6175993122ce79e9abe7d8c1f5c59b38c
RajithaY <rajithax.yerrumsetty@intel.com>
2017-04-25 03:31:15 -0700

    Adding qemu as a submodule of KVMFORNFV
    This Patch includes the changes to add qemu as a submodule to
    kvmfornfv repo and make use of the updated latest qemu for the
    execution of all testcase
    Change-Id: I1280af507a857675c7f81d30c95255635667bdd7
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

--- ci/build_interface.sh
+++ ci/build_interface.sh
@@ -9,7 +9,10 @@ output_dir=$build_dir/build_output
 cp -r $build_dir $tmp_build_dir
 
 # Build qemu rpm packages
-cd $tmp_build_dir/qemu
+cd $tmp_build_dir
+git submodule init
+git submodule update --recursive
+cd qemu
 make clean
 ./configure
 

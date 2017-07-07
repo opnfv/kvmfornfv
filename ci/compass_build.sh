17b6d59ff4a211806387721b52702f55839a36af
kalyanreddy <reddyx.gundarapu@intel.com>
2017-07-07 12:34:20 +0530

    Generating debian packages for kernel
    This patch includes modification of ci scripts for generating the
    debian packages only for kernel. Once debug package is verified will
    revert default package_type to centos.
    
    Change-Id: I990129161313635075763ee3d80a43ed51ee3d37
    Signed-off-by: Gundarapu Kalyan Reddy <reddyx.gundarapu@intel.com>

---
+++ ci/compass_build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function checkout_commit() {
+build_dir=/opt/kvmfornfv/
+mkdir -p /tmp/kvmfornfv
+SRC=/tmp/kvmfornfv
+source ${build_dir}/ci/compass.conf
+cd $SRC
+#Cloning into /tmp/kvmfornfv from local repository
+git clone $build_dir $SRC
+if [ "$branch" == "master" ] || [ "$branch" == "danube" ];then
+   echo "Checking out on $branch branch"
+   echo "Commit-id is ${commit_id}"
+   git checkout -f ${commit_id}
+fi
+mkdir ${output_dir}
+}
+
+output_dir="$1"
+checkout_commit
+
+kernel_src_dir=$SRC/kernel
+config_file="${kernel_src_dir}/arch/x86/configs/opnfv.config"
+
+if [ ! -d ${output_dir} -o ! -w ${output_dir} ] ; then
+    echo "${0}: Output directory '${output_dir}' does not exist or cannot \
+          be written"
+    exit 1
+fi
+
+if [ ! -d ${kernel_src_dir} ] ; then
+    echo "${0}: Directory '${kernel_src_dir}' does not exist, run this script \
+          from the root of kvmfornfv source tree"
+    exit 1
+fi
+
+echo
+echo "Build"
+echo
+
+cp -f ${config_file} "${kernel_src_dir}/.config"
+cd $kernel_src_dir
+# Workaround build bug on Ubuntu 14.04
+cat <<EOF > arch/x86/boot/install.sh
+#!/bin/sh
+cp -a -- "\$2" "\$4/vmlinuz-\$1"
+EOF
+
+make-kpkg clean
+fakeroot make-kpkg --initrd --revision=$VERSION kernel_image kernel_headers kernel_debug -j$(nproc)
+make 
+if [ ${?} -ne 0 ] ; then
+    echo "Kernel build failed"
+    exit 1
+fi
+
+echo "print the built debains"
+ls -lrth /root/kvmfornfv/
+mv /root/kvmfornfv/* /root/kvmfornfv/build_output
+#cleaning the /tmp
+rm -rf ${SRC}
